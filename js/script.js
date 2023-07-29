14a991d78900f7b6d475dc40a9aabdc2765d264e
Lashtabeg <tahcik@live.com>
2023-07-29 18:47:50 +0800

    29.09.2023 Mentor

--- js/script.js
+++ js/script.js
@@ -149,34 +149,34 @@
 //   DARK: "dark-theme",
 // };
 
-const input = document.getElementById("theme-switch-toggle");
-const body = document.body;
+// const input = document.getElementById("theme-switch-toggle");
+// const body = document.body;
 
-input.addEventListener("change", onChange);
+// input.addEventListener("change", onChange);
 
-const savedTheme = localStorage.getItem("Theme");
-// console.log(savedTheme);
+// const savedTheme = localStorage.getItem("Theme");
+// // console.log(savedTheme);
 
-if (savedTheme) {
-  body.classList.add(savedTheme);
-  if (savedTheme === Theme.DARK) {
-    input.checked = true;
-  }
-} else {
-  body.classList.add(Theme.LIGHT);
-}
+// if (savedTheme) {
+//   body.classList.add(savedTheme);
+//   if (savedTheme === Theme.DARK) {
+//     input.checked = true;
+//   }
+// } else {
+//   body.classList.add(Theme.LIGHT);
+// }
 
-function onChange(e) {
-  if (e.currentTarget.checked) {
-    body.classList.add(Theme.DARK);
-    body.classList.remove(Theme.LIGHT);
-    localStorage.setItem("Theme", Theme.DARK);
-  } else {
-    body.classList.add(Theme.LIGHT);
-    body.classList.remove(Theme.DARK);
-    localStorage.setItem("Theme", Theme.LIGHT);
-  }
-}
+// function onChange(e) {
+//   if (e.currentTarget.checked) {
+//     body.classList.add(Theme.DARK);
+//     body.classList.remove(Theme.LIGHT);
+//     localStorage.setItem("Theme", Theme.DARK);
+//   } else {
+//     body.classList.add(Theme.LIGHT);
+//     body.classList.remove(Theme.DARK);
+//     localStorage.setItem("Theme", Theme.LIGHT);
+//   }
+// }
 
 //===================================================
 // * Якщо імейл і пароль користувача збігаються, при сабміті зберігай дані з форми
